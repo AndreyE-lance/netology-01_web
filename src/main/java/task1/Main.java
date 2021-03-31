bddcd4fab8cfa869dd14e154fdd482b5b3ef7a02
Andrey <erkcasb@gmail.com>
2021-03-31 18:22:35 +0500

    extract method, add new handler

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -11,22 +11,7 @@ public class Main {
         server.addHandler("GET", "/index.html", new Handler() {
             public void handle(Request request, BufferedOutputStream responseStream) {
                 // TODO: handlers code
-                final var filePath = Path.of("src", "public", request.getPath());
-                final String mimeType;
-                try {
-                    mimeType = Files.probeContentType(filePath);
-                    final var length = Files.size(filePath);
-                    responseStream.write(("HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-                    ).getBytes());
-                    Files.copy(filePath, responseStream);
-                    responseStream.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                sendRespond(request, responseStream);
 
             }
         });
@@ -34,24 +19,36 @@ public class Main {
         server.addHandler("GET", "/spring.html", new Handler() {
             public void handle(Request request, BufferedOutputStream responseStream) {
                 // TODO: handlers code
-                final var filePath = Path.of("src", "public", request.getPath());
-                final String mimeType;
-                try {
-                    mimeType = Files.probeContentType(filePath);
-                    final var length = Files.size(filePath);
-                    responseStream.write(("HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-                    ).getBytes());
-                    Files.copy(filePath, responseStream);
-                    responseStream.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                sendRespond(request, responseStream);
             }
         });
+
+        server.addHandler("GET", "/main/main.html", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+                sendRespond(request, responseStream);
+            }
+        });
+
         server.listen(53535);
+    }
+
+    public static void sendRespond(Request request, BufferedOutputStream responseStream) {
+        final var filePath = Path.of("src", "public", request.getPath());
+        final String mimeType;
+        try {
+            mimeType = Files.probeContentType(filePath);
+            final var length = Files.size(filePath);
+            responseStream.write(("HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: " + mimeType + "\r\n" +
+                    "Content-Length: " + length + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n"
+            ).getBytes());
+            Files.copy(filePath, responseStream);
+            responseStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
